5de8ed1f4d484c2711fd59610bac380ae33bc085
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-05 17:58:42 +0200

    Added Stars

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,6 +9,12 @@ export default {
         }
     },
     components: { CardMain },
+    methods: {
+        getVote(vote) {
+            const newVote = Math.ceil(vote / 2)
+            return newVote
+        }
+    }
 }
 </script>
 
@@ -19,13 +25,13 @@ export default {
             <h2 v-if="store.movies.length">Movies</h2>
             <div class="row row-cols-3">
                 <CardMain v-for="movie in store.movies" :key="movie.id" :title="movie.title"
-                    :originalTitle="movie.original_title" :language="movie.original_language" :vote="movie.vote_average"
-                    :img="this.url + movie.poster_path" />
+                    :originalTitle="movie.original_title" :language="movie.original_language"
+                    :vote="getVote(movie.vote_average)" :img="this.url + movie.poster_path" />
             </div>
             <h2 v-if="store.tvSeries.length">TV Series</h2>
             <div class="row row-cols-3">
                 <CardMain v-for=" tv  in  store.tvSeries " :key="tv.id" :title="tv.name" :originalTitle="tv.original_name"
-                    :language="tv.original_language" :vote="tv.vote_average" :img="this.url + tv.poster_path" />
+                    :language="tv.original_language" :vote="getVote(tv.vote_average)" :img="this.url + tv.poster_path" />
             </div>
         </div>
     </main>
--- src/components/main-components/CardMain.vue
+++ src/components/main-components/CardMain.vue
@@ -25,7 +25,17 @@ export default {
             <li>{{ title }}</li>
             <li>{{ originalTitle }}</li>
             <li><img :src="getImagePath(language)" :alt="language"></li>
-            <li>{{ vote }}</li>
+            <li>
+                <ol>
+                    <li>
+                        <i class="fa-regular fa-star" :class="{ 'fa-solid': vote > 0 }"></i>
+                        <i class="fa-regular fa-star" :class="{ 'fa-solid': vote > 1 }"></i>
+                        <i class="fa-regular fa-star" :class="{ 'fa-solid': vote > 2 }"></i>
+                        <i class="fa-regular fa-star" :class="{ 'fa-solid': vote > 3 }"></i>
+                        <i class="fa-regular fa-star" :class="{ 'fa-solid': vote > 4 }"></i>
+                    </li>
+                </ol>
+            </li>
         </ol>
     </div>
 </template>
