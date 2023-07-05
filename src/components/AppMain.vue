b325b11fd8572a581344b31d14b75af165a94e18
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-05 16:07:26 +0200

    Added Milestone 1

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,26 @@
-<script></script>
+<script>
+import CardMain from "./main-components/CardMain.vue"
+import { store } from "../assets/data/store";
+export default {
+    data() {
+        return {
+            store
+        }
+    },
+    components: { CardMain },
+}
+</script>
 
 
 <template>
-    <main></main>
+    <main>
+        <div class="container">
+            <div class="row">
+                <CardMain v-for="movie in store.movies" :title="movie.title" :originalTitle="movie.original_title"
+                    :language="movie.original_language" :vote="movie.vote_average" />
+            </div>
+        </div>
+    </main>
 </template>
 
 <style scoped lang="scss">
---
+++ src/components/main-components/CardMain.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    props: {
+        title: String,
+        originalTitle: String,
+        language: String,
+        vote: Number,
+    }
+}
+</script>
+
+
+
+<template>
+    <div class="col">
+        <ol>
+            <li>{{ title }}</li>
+            <li>{{ originalTitle }}</li>
+            <li>{{ language }}</li>
+            <li>{{ vote }}</li>
+        </ol>
+    </div>
+</template>
+
+
+
+<style scoped lang="scss">
+li {
+    color: white;
+}
+</style>
